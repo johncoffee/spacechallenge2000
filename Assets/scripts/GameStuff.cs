656589c6282b51d2af7ffe800aaf679f407ef729
Julian <julian@contingent.dk>
2015-10-25 14:37:48 +0100

    kinda works

---
+++ Assets/scripts/GameStuff.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameStuff : MonoBehaviour {
+
+	public int number = 10;
+	public int radius = 10;
+	public GameObject misslePrefab;
+	public GameObject target;
+	public float speed = 3f;
+
+	void Start () {
+//		Spawn();
+	}
+
+	void Spawn() {
+		for (int i =0; i< number; i++) {
+			GameObject missle = (GameObject) Instantiate(misslePrefab);
+			missle.transform.position = new Vector3(
+				-radius + (Random.value * 2 * radius),
+				-radius + (Random.value * 2 * radius),
+				-radius + (Random.value * 2 * radius)
+			);
+
+			Vector3 targetPos = target.transform.position;		
+			Vector3 velocity = targetPos - missle.transform.position;
+			velocity = velocity.normalized * speed;
+			Debug.Log (velocity);
+			missle.GetComponent<Rigidbody>().velocity = velocity;
+
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			Spawn();
+		}
+	}
+}
---
+++ Assets/scripts/SimpleGun.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleGun : MonoBehaviour {
+
+	float cooldown = 1.5f;
+	float lastShot = 0f;
+
+	public float speed = 5f;
+
+	public GameObject spawnHere;
+
+	public GameObject bulletPrefab;
+
+	// Use this for initialization
+	void Start () {
+		lastShot = -cooldown;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.A)) {
+			int radius = 10;
+			ShootAt(
+				new Vector3(
+				-radius + (Random.value * 2 * radius),
+				-radius + (Random.value * 2 * radius),
+				-radius + (Random.value * 2 * radius)
+				)
+			);
+		}
+	}
+
+	void ShootAt (Vector3 target) {
+		GameObject bullet = (GameObject) Instantiate(bulletPrefab);
+		bullet.transform.position = spawnHere.transform.position;
+	
+		Vector3 velocity = target - transform.position;
+		velocity = velocity.normalized * speed;
+		bullet.GetComponent<Rigidbody>().velocity = velocity;		
+	}
+	
+}
