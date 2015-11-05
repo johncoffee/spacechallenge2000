58a69f579d57080b74cffa665f465e573a8cca01
Julian <julian@contingent.dk>
2015-11-05 16:21:32 +0100

    level 1

--- Assets/ship/Ship.cs
+++ Assets/ship/Ship.cs
@@ -5,11 +5,13 @@ public class Ship : MonoBehaviour {
 
 	public float missleDamage = 20f;
 	public float shieldHp = 100f;
-	public ParticleSystem damageEffects;
+	public ParticleSystem damageEffects = null;
 
 	// Use this for initialization
 	void Start () {
-	
+		if (damageEffects == null) {
+			Debug.LogWarning("Remember to set a ParticleSystem on damageEffects");
+		}
 	}
 	
 	// Update is called once per frame
